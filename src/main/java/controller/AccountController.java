03df2f86d3804b22ee31836385035092d2b46be1
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-10-24 16:45:05 +0000

    began

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -1,7 +1,25 @@
 package controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import models.Users;
+import service.AccountService;
+
+import javax.ws.rs.*;
+import java.util.ArrayList;
+
 /**
  * Created by VilhjalmurAlex on 24/10/2016.
  */
+@Path("/api")
 public class AccountController {
+    @GET
+    @Path("/users")
+    @Produces("application/json")
+    public ArrayList<Users> getHelloWorld() throws JsonProcessingException {
+        System.out.println("In the account endpoint");
+
+        ArrayList<Users> users = AccountService.getUsers();
+
+        return users;
+    }
 }
--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -1,7 +1,23 @@
 package service;
 
+import java.util.ArrayList;
+import models.Users;
+
 /**
  * Created by VilhjalmurAlex on 24/10/2016.
  */
 public class AccountService {
+    private static ArrayList<Users> users;
+
+    public AccountService(){
+        users = new ArrayList<>();
+    }
+
+    public void addUsers(Users user) {
+        users.add(user);
+    }
+
+    public static ArrayList<Users> getUsers() {
+        return users;
+    }
 }
